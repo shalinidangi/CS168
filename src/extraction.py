4898f3e7364f3f651e20945b57a32ed32a532f1c
Kelvin Zhang <k.zhang661@gmail.com>
2018-06-03 17:13:28 -0700

    remove any patches that contain 0

--- src/extraction.py
+++ src/extraction.py
@@ -10,15 +10,17 @@ from sklearn.metrics import accuracy_score
 
 def extract_features(image, mask, n):
     patches = []
+    trimmed_mask = []
 
     for i in range(n, 255-n):
         for j in range(n, 255-n):
-            if mask[i, j].astype(int) != 0:
+            if 0 not in mask[i-n:i+n+1, j-n:j+n+1].astype(int):
                 patch = image[i-n:i+n+1, j-n:j+n+1]
-                patches.append(patch.flatten())
+                patches.append(patch.flatten())   
+                trimmed_mask.append(int(mask[i,j]))             
 
     # print(patches)
-    return patches
+    return patches, trimmed_mask
     
 
 def flatten_mask(mask, n):
@@ -47,7 +49,7 @@ def create_training_set(test_patient):
 
     for i in range(0, 62):
         # Gather masks and images for all patients (excluding test patient)
-    	if i == test_patient:
+    	if i == test_patient or i == 27:
     		continue
     	masks.append(data[i][0])
     	t2_images.append(data[i][1])
@@ -58,15 +60,14 @@ def create_training_set(test_patient):
 
     for i in range(len(masks)):
         # Extract features for all patients
-        patient = extract_features(t2_images[i], masks[i], 3)
+        patient, trimmed_mask = extract_features(t2_images[i], masks[i], 6)
         for j in range(len(patient)):
             feature_vectors.append(patient[j])
 
         # Flatten and reduce masks for use in accuracy and AUC testing
         accuracy_mask = flatten_mask(masks[i], 3).tolist()
-        auc_mask = reduce_mask(accuracy_mask)
         # mask_vectors += accuracy_mask
-        auc_vectors += auc_mask
+        auc_vectors += reduce_mask(trimmed_mask)
         # print(auc_mask)
 
     print(len(feature_vectors))
@@ -91,13 +92,12 @@ def build_model(feature_vectors, auc_vectors, classifier, filename, train=False)
 def test_model(test_patient, model):
     # Determine features and masks for test patient
     test_mask = data[test_patient][0]
-    test_features = extract_features(data[test_patient][1], test_mask, 3)
-    auc_test_mask = reduce_mask(flatten_mask(test_mask,3))
-    # print(auc_test_mask)
+    test_features, trimmed_test_mask = extract_features(data[test_patient][1], test_mask, 6)
 
     pred = model.predict(test_features)
     
-    print(roc_auc_score(y_true=auc_test_mask, y_score=pred))
+    print(trimmed_test_mask)
+    print(roc_auc_score(y_true=reduce_mask(trimmed_test_mask), y_score=pred))
     # print(accuracy_score(y_true=test_mask, y_pred=pred))
 
 
@@ -110,10 +110,10 @@ def run_model(test_patient):
     feature_vectors, auc_vectors = create_training_set(test_patient)
     # print(feature_vectors)
     # print(auc_vectors)
-    model = build_model(feature_vectors, auc_vectors, classifier1, "forest.sav", True)
+    model = build_model(feature_vectors, auc_vectors, classifier3, "gbc.sav", True)
     test_model(test_patient, model)
 
 
-run_model(8)
+run_model(18)
 
 
